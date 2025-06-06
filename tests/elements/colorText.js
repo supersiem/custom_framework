bc0c1d66414a3d40aa751e332527fe95365dcf49
Siem <vankeulensiem@gmail.com>
2025-06-06 17:43:04 +0200

    add groen omdat

--- tests/elements/colorText.js
+++ tests/elements/colorText.js
@@ -3,10 +3,18 @@ const colorText = {
         {
             "tag": "rood",
             "code": rood
+        },
+        {
+            "tag": "groen",
+            "code": groen
         }
     ]
 };
 function rood(element) {
     element.innerHTML = `<span style="color: red;">${element.innerHTML}</span>`;
+    changeTag(element, 'p');
+}
+function groen(element) {
+    element.innerHTML = `<span style="color: green;">${element.innerHTML}</span>`;
     changeTag(element, 'p');
 }
